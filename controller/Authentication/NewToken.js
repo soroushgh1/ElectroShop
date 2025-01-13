bbd299629f58582669e429e52a072d90448b60f1
soroushGH <soroushdeveloper20@gmail.com>
2025-01-13 23:29:34 +0300

    added new accesstoken mechanism.

---
+++ controller/Authentication/NewToken.js
@@ -0,0 +1,25 @@
+import prisma from "../../prismaclient";
+import jwt from "jsonwebtoken";
+
+export const NewToken = async (req, res) => {
+
+    try {
+
+        const accessToken = req.cookies.accessToken;
+        const refreshToken = req.cookies.refreshToken;
+
+        if(!refreshToken) return res.status(400).json({ error: "Login required." });
+
+        const user = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+
+        const newToken = jwt.sign({ email: user.email }, process.env.JWT_ACCESS_SECRET, { expiresIn: '15m' });
+
+        res.cookie("accessToken", newToken, { httpOnly : true });
+
+        return res.status(200).json({ message: "New access token set" });
+
+    } catch (err) {
+        
+        return res.status(400).json({ error: err.message });
+    }
+}
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -2,14 +2,19 @@ import { Router } from "express";
 import { RegisterUsers } from "../controller/Authentication/RegisterUsers.js";
 import { LoginUsers } from "../controller/Authentication/LoginUsers.js";
 import { AuthStatus } from "../controller/Authentication/AuthStatus.js";
+import { NewToken } from "../controller/Authentication/NewToken.js";
+
 
 const router = Router();
+
 
 router.post("/register", RegisterUsers);
 
 router.post("/login", LoginUsers);
 
 router.post("/authstatus", AuthStatus);
+
+router.post("/newtoken", NewToken);
 
 
 export default router;
